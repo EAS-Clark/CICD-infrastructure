1e0fe47698587aa61663f0b48500099633cbe497
EAS-Clark <clark@enterpriseautomation.co.uk>
2023-07-18 14:00:10 +0100

    updated pathing for installing scrpit and move files

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 
 # install tekton
-kubectl apply -f task/tekton.yaml
+kubectl apply -f install/task/tekton.yaml
 
 # install nexus3
 helm repo add stevehipwell https://stevehipwell.github.io/helm-charts/
-helm upgrade --install --namespace default --values ./values/values-nexus3.yaml my-release stevehipwell/nexus3
+helm upgrade --install --namespace default --values ./install/values/values-nexus3.yaml my-release stevehipwell/nexus3
 
 # install sonarqube
 helm repo add sonarqube https://SonarSource.github.io/helm-chart-sonarqube
-helm upgrade --install --namespace default --values ./values/values-sonarqube.yaml sonarqube sonarqube/sonarqube
+helm upgrade --install --namespace default --values ./install/values/values-sonarqube.yaml sonarqube sonarqube/sonarqube
 
 # install vault
 helm repo add hashicorp https://helm.releases.hashicorp.com
@@ -19,9 +19,9 @@ helm repo add argo https://argoproj.github.io/argo-helm
 helm install my-release argo/argocd-apps
 
 # add trivy-scanner
-kubectl apply -f task/trivy-scanner.yaml
+kubectl apply -f install/task/trivy-scanner.yaml
 
 # install buildah
-kubectl apply -f task/buildah.yaml
+kubectl apply -f install/task/buildah.yaml
 
 
